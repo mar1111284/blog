f3f2d67e29b67d584b8d1053c3e0be2008496613
Rekav <marindurand29@icloud.com>
2026-01-14 13:16:14 +1100

    create a build

--- js/index.js
+++ js/index.js
@@ -1,12 +1,11 @@
 // js/index.js
 
-
 document.addEventListener('DOMContentLoaded', () => {
-
     // Check if we have active root session from this tab
     if (sessionStorage.getItem('rekav_root_session') === 'true') {
         window.REKAV_ROOT = true;
     } else {
         window.REKAV_ROOT = false;
-    
+    }
 });
+
